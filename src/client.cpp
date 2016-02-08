9da13963433ec2f57e68859ed0455385b00724c4
Kai Sommerfeld <kai.sommerfeld@gmx.com>
2016-02-08 16:22:32 +0100

    [API 5.0.0] Remove GUI API dependency

--- src/client.cpp
+++ src/client.cpp
@@ -24,7 +24,6 @@
 
 #include "client.h"
 #include "kodi/xbmc_pvr_dll.h"
-#include "kodi/libKODI_guilib.h"
 #include "PVRIptvData.h"
 #include "p8-platform/util/util.h"
 
@@ -293,12 +292,12 @@ const char* GetMininumPVRAPIVersion(void)
 
 const char* GetGUIAPIVersion(void)
 {
-  return KODI_GUILIB_API_VERSION;
+  return ""; // GUI API not used
 }
 
 const char* GetMininumGUIAPIVersion(void)
 {
-  return KODI_GUILIB_MIN_API_VERSION;
+  return ""; // GUI API not used
 }
 
 PVR_ERROR GetAddonCapabilities(PVR_ADDON_CAPABILITIES* pCapabilities)
