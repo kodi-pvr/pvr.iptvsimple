c15f6e067e9b5729b11d488bd968857a85670122
phunkyfish <phunkyfish@gmail.com>
2019-09-23 13:52:23 +0100

    refactoring

--- src/iptvsimple/data/EpgEntry.cpp
+++ src/iptvsimple/data/EpgEntry.cpp
@@ -311,12 +311,12 @@ bool EpgEntry::ParseXmltvNsEpisodeNumberInfo(const std::string& episodeNumberStr
 
 bool EpgEntry::ParseOnScreenEpisodeNumberInfo(const std::string& episodeNumberString)
 {
-  static const std::regex numRegex("[ \\txX_\\.]");
-  const std::string text = std::regex_replace(episodeNumberString, numRegex, "");
+  static const std::regex unwantedCharsRegex("[ \\txX_\\.]");
+  const std::string text = std::regex_replace(episodeNumberString, unwantedCharsRegex, "");
 
   std::smatch match;
-  static const std::regex epRegex("^[sS]([0-9][0-9]*)[eE][pP]?([0-9][0-9]*)$");
-  if (std::regex_match(text, match, epRegex))
+  static const std::regex seasonEpisodeRegex("^[sS]([0-9][0-9]*)[eE][pP]?([0-9][0-9]*)$");
+  if (std::regex_match(text, match, seasonEpisodeRegex))
   {
     if (match.size() == 3)
     {
