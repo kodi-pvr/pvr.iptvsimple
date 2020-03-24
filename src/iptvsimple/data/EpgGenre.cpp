666164c1db0d2fb1901bccdd0d54eecb5e42eaa9
Alwin Esch <alwin.esch@web.de>
2020-03-24 18:33:25 +0100

    update source license to 2020 and use Kodi / kodi.tv

--- src/iptvsimple/data/EpgGenre.cpp
+++ src/iptvsimple/data/EpgGenre.cpp
@@ -1,6 +1,6 @@
 /*
- *      Copyright (C) 2005-2019 Team XBMC
- *      http://xbmc.org
+ *      Copyright (C) 2005-2020 Team Kodi
+ *      https://kodi.tv
  *
  *  This Program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -13,7 +13,7 @@
  *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
- *  along with XBMC; see the file COPYING.  If not, write to
+ *  along with Kodi; see the file COPYING.  If not, write to
  *  the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,
  *  MA 02110-1335, USA.
  *  http://www.gnu.org/copyleft/gpl.html
