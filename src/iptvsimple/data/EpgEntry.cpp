10b8d22a77676c104781ee915b3071c40c4a2aed
Alwin Esch <alwin.esch@web.de>
2021-09-18 18:39:58 +0200

    update copyright year to 2021

--- src/iptvsimple/data/EpgEntry.cpp
+++ src/iptvsimple/data/EpgEntry.cpp
@@ -1,6 +1,5 @@
 /*
- *  Copyright (C) 2005-2020 Team Kodi
- *  https://kodi.tv
+ *  Copyright (C) 2005-2021 Team Kodi (https://kodi.tv)
  *
  *  SPDX-License-Identifier: GPL-2.0-or-later
  *  See LICENSE.md for more information.
