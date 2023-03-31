fb4b981beefa70e896018780656b749b8bfd21f7
phunkyfish <phunkyfish@gmail.com>
2023-03-31 14:33:24 +0200

    Revert "Support async connect"
    This reverts commit 34a0b8370efc860190518ee5ff1ab9f23af7cdbe.

--- src/iptvsimple/PlaylistLoader.cpp
+++ src/iptvsimple/PlaylistLoader.cpp
@@ -517,7 +517,7 @@ void PlaylistLoader::ParseSinglePropertyIntoChannel(const std::string& line, Cha
   }
 }
 
-bool PlaylistLoader::ReloadPlayList()
+void PlaylistLoader::ReloadPlayList()
 {
   m_m3uLocation = m_settings->GetM3ULocation();
 
@@ -532,15 +532,11 @@ bool PlaylistLoader::ReloadPlayList()
     m_client->TriggerChannelGroupsUpdate();
     m_client->TriggerProvidersUpdate();
     m_client->TriggerRecordingUpdate();
-
-    return true;
   }
   else
   {
     m_channels.ChannelsLoadFailed();
     m_channelGroups.ChannelGroupsLoadFailed();
-
-    return false;
   }
 }
 
